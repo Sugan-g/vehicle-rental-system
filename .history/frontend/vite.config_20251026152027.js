0405eb429413a6c577fe8e4b4e822bda148ad45b
Sugan-g <suganvasu1993@gmail.com>
2025-11-12 16:24:26 +0530

    Add redirects and Vite config for Netlify

--- .history/frontend/vite.config_20251026152027.js
+++ .history/frontend/vite.config_20251026152027.js
@@ -2,12 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  build: {
-    outDir: 'dist', // output folder for Netlify
-  },
-  server: {
-    port: 5173,
-  },
 })
