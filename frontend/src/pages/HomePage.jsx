ff4f1a9f2c2a2906008870b5146393fe10c69ad0
Sugan-g <suganvasu1993@gmail.com>
2025-11-25 13:31:25 +0530

    changes done

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -90,7 +90,7 @@ export default function HomePage() {
             )}
 
             {/*  ADMIN ONLY PIE CHART */}
-            {isAdmin && <DashboardCharts />} 
+            {/* {isAdmin && <DashboardCharts />}  */}
         </div>
     );
 }
